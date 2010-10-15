02244a7c45f7e4059d6021167c00da63b07c12e6
Kevin Smith <kevin@hypotheticalabs.com>
2010-10-15 08:53:52 -0400

    Updating erl_nif_compat to a R14-compatible version

--- c_src/erl_nif_compat.h
+++ c_src/erl_nif_compat.h
@@ -1,24 +1,3 @@
-// -------------------------------------------------------------------
-//
-// hammy: An Erlang binding for hamsterdb
-//
-// Copyright (c) 2010 Basho Technologies, Inc. All Rights Reserved.
-//
-// This file is provided to you under the Apache License,
-// Version 2.0 (the "License"); you may not use this file
-// except in compliance with the License.  You may obtain
-// a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-//
-// -------------------------------------------------------------------
 #ifndef ERL_NIF_COMPAT_H_
 #define ERL_NIF_COMPAT_H_
 
@@ -44,7 +23,6 @@ extern "C" {
     enif_open_resource_type(E, NULL, N, D, F, T)
 
 #define enif_alloc_resource_compat(E, T, S) \
-
     enif_alloc_resource(T, S)
 
 #define enif_release_resource_compat(E, H) \
